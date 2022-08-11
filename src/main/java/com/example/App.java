3b42ebf329b83c40515d35b4ba7652a9c5caaa73
DeadWalrus <pointlesstack@gmail.com>
2022-08-11 16:30:36 -0400

    make UI the main class

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -3,22 +3,13 @@ package com.example;
 
 import java.util.Scanner;
 
-public final class App {
-    private App() {
-    }
+public class App {
 
     /**
      * Finds factors and perfect squares for inputted number
      *
      * @param args The arguments of the program.
      */
-    
-    public static void main(String[] args) {
-        boolean running = true;
-        Scanner input = new Scanner(System.in); // Declare scanner in main. When scanner closes, it closes system.in
-        loop(input, running);
-        input.close();
-    }
 
 
     public static void loop(Scanner input, boolean running){
@@ -27,7 +18,7 @@ public final class App {
             //ADD QUICK MODE WHERE IT DOESN'T ASK EVERYTIME
             System.out.println("Would you like to:\nFind factors[1]?\nFind perfect squares[2]?\nBof[3]?\nExit[0]?");
 
-            int answer = UI.GetIntInput(input);
+            int answer = UI.getIntInput(input);
 
             if (answer == 0) {
                 System.out.println("Goodbye!");
@@ -40,17 +31,17 @@ public final class App {
             }
 
             System.out.print("Enter number ");
-            int num = UI.GetIntInput(input);
+            int num = UI.getIntInput(input);
             switch (answer) {
                 case 1:
-                    Faps.PrintFactors(num);
+                    Faps.printFactors(num);
                     break;
                 case 2:
-                    Faps.PrintPerfectSquares(num);
+                    Faps.printPerfectSquares(num);
                     break;
                 case 3:
-                    Faps.PrintFactors(num);
-                    Faps.PrintPerfectSquares(num);
+                    Faps.printFactors(num);
+                    Faps.printPerfectSquares(num);
                     break;
                 default:
                     System.out.println("Something went wrong. Try again.");
--- src/main/java/com/example/UI.java
+++ src/main/java/com/example/UI.java
@@ -1,12 +1,24 @@
 package com.example;
 import java.util.Scanner;
 import java.util.InputMismatchException;
-
+import javafx.application.Application;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
 /*
  * Class responsible for all gui elements
  */
-public class UI {
-    public static int GetIntInput(Scanner input) {
+public final class UI extends Application{
+    public static void main(String[] args) {
+        launch(args);
+    }
+    public void start(Stage primaryStage){
+        BorderPane pane = new BorderPane();
+        Scene scene = new Scene(pane);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+    public static int getIntInput(Scanner input) {
         boolean gotInput = false;
         int in = 0;
 
