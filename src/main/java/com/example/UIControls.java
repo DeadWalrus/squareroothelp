65a75307abcedd4127c775cc762488657c31657f
DeadWalrus <pointlesstack@gmail.com>
2022-08-16 11:23:06 -0400

    refactor and add checks

--- src/main/java/com/example/UIControls.java
+++ src/main/java/com/example/UIControls.java
@@ -4,19 +4,39 @@ import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Alert.AlertType;
+import javafx.scene.input.MouseEvent;
 
 public class UIControls {
+    /*
+     * Find factors and perfect squares, then output to textAreas
+     */
     public static EventHandler<ActionEvent> findFactorsAndPerfectSquares = (ActionEvent e) ->{
         int [][] factors = null;
         int [] squares = null;
+        int input = 0;
+
+        // Check if textField is empty
+        if(UIComponents.textFields.get("Enter number").getText().equals("")) {
+            Alert alert = new Alert(AlertType.ERROR, "Please enter a number");
+            alert.showAndWait();
+            System.out.println("TextField empty, please enter a number");
+            return;
+        }
+
+        // Try to convert textField string to int
         try {
-            factors = Faps.findFactors(Integer.parseInt(UIComponents.textFields.get("Enter number").getText()));
-            squares = Faps.findPerfectSquares(Integer.parseInt(UIComponents.textFields.get("Enter number").getText()));
+            input = Integer.parseInt(UIComponents.textFields.get("Enter number").getText());
         } catch (NumberFormatException ex) {
             Alert alert = new Alert(AlertType.ERROR, "Could not convert from string to int");
             alert.showAndWait();
             System.out.println("Could not convert from int to String");
+            return;
         }
+        
+        
+        factors = Faps.findFactors(input);
+        squares = Faps.findPerfectSquares(input);
+        
 
         // Clear default text in text areas
         UIComponents.textAreas.get("Factors").clear();
@@ -37,5 +57,9 @@ public class UIControls {
             UIComponents.textAreas.get("Perfect Squares").appendText("\n");
 
         }
+    };
+
+    public static EventHandler<MouseEvent> textFieldClear = (MouseEvent e) -> {
+        UIComponents.textFields.get("Enter number").clear();
     };
 }
