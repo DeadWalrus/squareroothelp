c7e006402e73ce139a317bb5881531933ade8260
DeadWalrus <pointlesstack@gmail.com>
2022-08-20 21:27:23 -0400

    refactor code

--- src/main/java/com/example/Faps.java
+++ src/main/java/com/example/Faps.java
@@ -11,11 +11,12 @@ class Faps {
     public static ArrayList<ArrayList<BigInteger>> findFactors(BigInteger num) {
         System.out.println(num);
         ArrayList<ArrayList<BigInteger>> factors = new ArrayList<ArrayList<BigInteger>>();
-        
+
         for (BigInteger i = BigInteger.valueOf(1); num.compareTo(i) > 0; i = i.add(BigInteger.ONE)) {
             if (num.mod(i).equals(BigInteger.ZERO)) {
                 if (factors.size() >= 1) {
-                    if (i.equals(factors.get(factors.size()-1).get(1)) || factors.get(factors.size()-1).get(0).equals( factors.get(factors.size()-1).get(1))){
+                    if (i.equals(factors.get(factors.size() - 1).get(1))
+                            || factors.get(factors.size() - 1).get(0).equals(factors.get(factors.size() - 1).get(1))) {
                         break;
                     }
                 }
@@ -27,6 +28,7 @@ class Faps {
         }
         return factors;
     }
+
     /*
      * Find the perfect squares and return an array containing those squares
      */
@@ -34,11 +36,14 @@ class Faps {
 
         ArrayList<BigInteger> squares = new ArrayList<BigInteger>();
         ArrayList<ArrayList<BigInteger>> factors = fact;
-        for (BigInteger i = BigInteger.valueOf(0); i.compareTo(BigInteger.valueOf(factors.size())) < 0; i = i.add(BigInteger.ONE)) {
-            for (BigInteger j = BigInteger.valueOf(0); j.compareTo(BigInteger.valueOf(factors.get(0).size())) < 0; j = j.add(BigInteger.ONE)) {
+        for (BigInteger i = BigInteger.valueOf(0); i.compareTo(BigInteger.valueOf(factors.size())) < 0; i = i
+                .add(BigInteger.ONE)) {
+            for (BigInteger j = BigInteger.valueOf(0); j.compareTo(BigInteger.valueOf(factors.get(0).size())) < 0; j = j
+                    .add(BigInteger.ONE)) {
                 BigInteger p = factors.get(i.intValue()).get(j.intValue());
-                if (p.sqrt().pow(2).equals(p)){
-                    if (BigInteger.ZERO.compareTo(BigInteger.valueOf(squares.size())) < 0 && squares.get(squares.size()-1).equals(factors.get(i.intValue()).get(j.intValue()))){
+                if (p.sqrt().pow(2).equals(p)) {
+                    if (BigInteger.ZERO.compareTo(BigInteger.valueOf(squares.size())) < 0
+                            && squares.get(squares.size() - 1).equals(factors.get(i.intValue()).get(j.intValue()))) {
                         continue;
                     }
                     squares.add(factors.get(i.intValue()).get(j.intValue()));
