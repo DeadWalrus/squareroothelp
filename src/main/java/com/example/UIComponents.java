f3ffdcd57db8b196ea34293d297ce8159af31c15
DeadWalrus <pointlesstack@gmail.com>
2022-08-20 21:29:12 -0400

    refactor code

--- src/main/java/com/example/UIComponents.java
+++ src/main/java/com/example/UIComponents.java
@@ -6,12 +6,13 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
+
 public class UIComponents {
     protected static HashMap<String, Button> buttons = new HashMap<String, Button>();
     protected static HashMap<String, Label> labels = new HashMap<String, Label>();
     protected static HashMap<String, TextField> textFields = new HashMap<String, TextField>();
     protected static HashMap<String, TextArea> textAreas = new HashMap<String, TextArea>();
-    
+
     public static void newButton(String name) {
         buttons.put(name, new Button(name));
     }
