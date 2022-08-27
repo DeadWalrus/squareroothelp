2fccf82d7a025fee5ff957ac196eeed25aab617f
DeadWalrus <pointlesstack@gmail.com>
2022-08-27 16:23:59 -0400

    remove implements

--- src/main/java/com/nthroot/UI.java
+++ src/main/java/com/nthroot/UI.java
@@ -1,23 +1,5 @@
 package com.nthroot;
-import com.interfaces.UserInterface;
-public class UI implements UserInterface{
 
-    @Override
-    public void setLayout() {
-        // TODO Auto-generated method stub
-        
-    }
+public class UI{
 
-    @Override
-    public void declareComponentsAndSetProperties() {
-        // TODO Auto-generated method stub
-        
-    }
-
-    @Override
-    public void setControls() {
-        // TODO Auto-generated method stub
-        
-    }
-    
 }
