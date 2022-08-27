7017e1ef8ab6a90ebd10dbaa87f9e50d18eae7a4
DeadWalrus <pointlesstack@gmail.com>
2022-08-27 15:13:23 -0400

    change variables to private and methods to public

--- src/main/java/com/uicomponents/UIComponents.java
+++ src/main/java/com/uicomponents/UIComponents.java
@@ -9,29 +9,31 @@ import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 
 public class UIComponents {
-    protected HashMap<String, Button> buttons = null;
-    protected HashMap<String, Label> labels = null;
-    protected HashMap<String, TextField> textFields = null;
-    protected HashMap<String, TextArea> textAreas = null;
+    
+    private HashMap<String, Button> buttons = null;
+    private HashMap<String, Label> labels = null;
+    private HashMap<String, TextField> textFields = null;
+    private HashMap<String, TextArea> textAreas = null;
+
     public UIComponents() {
         this.buttons = new HashMap<>();
         this.labels = new HashMap<>();
         this.textFields = new HashMap<>();
         this.textAreas = new HashMap<>();
     }
-    protected void newButton(String name) {
+    public void newButton(String name) {
         buttons.put(name, new Button(name));
     }
 
-    protected void newLabel(String name) {
+    public void newLabel(String name) {
         labels.put(name, new Label(name));
     }
 
-    protected void newTextField(String name) {
+    public void newTextField(String name) {
         textFields.put(name, new TextField(name));
     }
 
-    protected void newTextArea(String name) {
+    public void newTextArea(String name) {
         textAreas.put(name, new TextArea());
     }
 
