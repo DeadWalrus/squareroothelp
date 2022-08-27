f0264c5ae0e1961c18f027472c2c5428230f104c
DeadWalrus <pointlesstack@gmail.com>
2022-08-27 14:53:16 -0400

    refactor for new UIComponents

--- src/main/java/com/faps/UIControls.java
+++ src/main/java/com/faps/UIControls.java
@@ -14,7 +14,7 @@ import javafx.scene.control.Alert.AlertType;
 import javafx.scene.input.MouseEvent;
 
 class UIControls {
-
+    static UIComponents comps = new UIComponents();
     /*
      * TODO
      * Create HashMap for controls
@@ -25,7 +25,7 @@ class UIControls {
         BigInteger input = BigInteger.valueOf(0);
 
         // Check if textField is empty
-        if (UIComponents.textFields.get("Enter number").getText().equals("")) {
+        if (comps.getTextField("Enter number").getText().equals("")) {
             Alert alert = new Alert(AlertType.ERROR, "Please enter a number");
             alert.showAndWait();
             System.out.println("TextField empty, please enter a number");
@@ -34,7 +34,7 @@ class UIControls {
 
         // Try to convert textField string to int
         try {
-            input = new BigInteger(UIComponents.textFields.get("Enter number").getText());
+            input = new BigInteger(comps.getTextField("Enter number").getText());
         } catch (NumberFormatException ex) {
             Alert alert = new Alert(AlertType.ERROR, "Could not convert from string to int");
             alert.showAndWait();
@@ -46,37 +46,37 @@ class UIControls {
         squares = Faps.findPerfectSquares(factors);
 
         // Clear default text in text areas
-        UIComponents.textAreas.get("Factors").clear();
-        UIComponents.textAreas.get("Perfect Squares").clear();
+        comps.getTextArea("Factors").clear();
+        comps.getTextArea("Perfect Squares").clear();
 
         // Append factors to text area
         for (BigInteger i = BigInteger.valueOf(0); i.compareTo(BigInteger.valueOf(factors.size())) < 0; i = i
                 .add(BigInteger.ONE)) {
             for (BigInteger j = BigInteger.valueOf(0); j.compareTo(BigInteger.valueOf(factors.get(0).size())) < 0; j = j
                     .add(BigInteger.ONE)) {
-                UIComponents.textAreas.get("Factors")
+                comps.getTextArea("Factors")
                         .appendText(factors.get(i.intValue()).get(j.intValue()).toString());
                 if (j.compareTo(BigInteger.ZERO) == 0)
-                    UIComponents.textAreas.get("Factors").appendText(", ");
+                    comps.getTextArea("Factors").appendText(", ");
             }
-            UIComponents.textAreas.get("Factors").appendText("\n");
+            comps.getTextArea("Factors").appendText("\n");
         }
 
         // Append perfect squares to text area
         for (BigInteger i = BigInteger.valueOf(0); i.compareTo(BigInteger.valueOf(squares.size())) < 0; i = i
                 .add(BigInteger.ONE)) {
             BigInteger square = squares.get(Integer.valueOf(i.toString()));
-            UIComponents.textAreas.get("Perfect Squares").appendText(square.toString());
-            UIComponents.textAreas.get("Perfect Squares").appendText("  \u21D2  " + square.sqrt().toString());
-            UIComponents.textAreas.get("Perfect Squares").appendText("\n");
+            comps.getTextArea("Perfect Squares").appendText(square.toString());
+            comps.getTextArea("Perfect Squares").appendText("  \u21D2  " + square.sqrt().toString());
+            comps.getTextArea("Perfect Squares").appendText("\n");
 
         }
     };
 
     // Clear text field if it contains only alpha characters
     protected static EventHandler<MouseEvent> textFieldClear = (MouseEvent e) -> {
-        System.out.println(UIComponents.textFields.get("Enter number").getText());
-        if (Pattern.matches("[^0-9]+", UIComponents.textFields.get("Enter number").getText()))
-            UIComponents.textFields.get("Enter number").clear();
+        System.out.println(comps.getTextField("Enter number").getText());
+        if (Pattern.matches("[^0-9]+", comps.getTextField("Enter number").getText()))
+            comps.getTextField("Enter number").clear();
     };
 }
